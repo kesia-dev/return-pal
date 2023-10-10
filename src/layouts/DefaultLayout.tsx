ee9aecb43f86963e2b7fb10fac6d0d9bd104074c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-10 19:50:37 -0400

    Cleanup tailwind class order for DefaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -6,7 +6,7 @@ export default function DefaultLayout({ children }: PropsWithChildren) {
   return (
     <>
       <div className="flex h-screen overflow-hidden">
-        <div className="bg-paleBlue relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden bg-paleBlue">
           <Header />
           <main className="max-w-9xl mx-auto w-full px-4 py-8 sm:px-6 lg:px-8">
             {children}
