217a7f0a00f07032502a790aed1460f5da0fb597
javascriptsucks <evix1988@live.cn>
2023-10-09 22:50:57 -0600

    remove next font dependency because it is in built in next 13

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 
 function Header() {
   return (
-    <div className=" font text flex h-16 w-full flex-row items-center justify-between bg-primary px-4 text-brand shadow-md">
+    <div className=" font text fixed left-0 top-0 flex h-16 w-full flex-row items-center justify-between bg-primary px-4 text-brand shadow-md">
       <h1 className="text-2xl font-bold">Reture-Pal</h1>
       <Button type="button">Login</Button>
     </div>
