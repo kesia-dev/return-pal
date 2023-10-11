95bf353a7d7b803ac3ae64f98bd52f6a90f63106
javascriptsucks <evix1988@live.cn>
2023-10-11 15:35:36 -0600

    merge from Layout changes

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,3 +1,35 @@
+// import { Nunito } from 'next/font/google'
+// import { type PropsWithChildren } from 'react'
+// import Footer from '@/components/Footer'
+// import Header from '@/components/Header'
+
+// const nunito = Nunito({
+//   weight: ['400', '500', '700', '900'],
+//   subsets: ['latin'],
+// })
+
+// export default function DefaultLayout({ children }: PropsWithChildren) {
+//   return (
+//     <>
+//       <div className="flex h-screen overflow-hidden">
+//         <div
+//           className={`bg-paleBlue relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden ${nunito.className}`}
+//         >
+//           <Header />
+//           <main className=" mx-auto w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+//             {children}
+//           </main>
+//           <Footer />
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export const getLayout = (page: React.ReactElement) => {
+//   return <DefaultLayout>{page}</DefaultLayout>
+// }
+
 import { Nunito } from 'next/font/google'
 import { type PropsWithChildren } from 'react'
 import Footer from '@/components/Footer'
@@ -11,14 +43,10 @@ const nunito = Nunito({
 export default function DefaultLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <div className="flex h-screen overflow-hidden">
-        <div
-          className={`bg-paleBlue relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden ${nunito.className}`}
-        >
+      <div className={`flex h-[100dvh] overflow-hidden ${nunito.className}`}>
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <Header />
-          <main className=" mx-auto w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-            {children}
-          </main>
+          <main className="grow">{children}</main>
           <Footer />
         </div>
       </div>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,7 +7,7 @@ function Contact() {
   return (
     // <div className="bg-paleBlue h-screen w-screen pl-10 pt-24">
     <>
-      <div className="flex max-w-lg flex-col">
+      <div className=" flex max-w-lg flex-col pl-10 pt-14">
         <h2 className="relative text-subtitle">
           <span className="mr-2 font-thin text-black">CONTACT</span>
           <span className="bg-gradient-to-r from-gradientL to-primary bg-clip-text font-bold text-transparent">
@@ -17,7 +17,7 @@ function Contact() {
         <ContactForm />
 
         <Image
-          className=" mt-4 self-center"
+          className="mt-4 self-center"
           src="/images/robot.png"
           alt="Contact Us"
           width={160}
@@ -25,7 +25,7 @@ function Contact() {
         />
       </div>
       <Image
-        className="mt-8"
+        className="my-4 pl-10"
         src="/images/navbar-logo-transparent.png"
         alt="Contact Us"
         width={160}
