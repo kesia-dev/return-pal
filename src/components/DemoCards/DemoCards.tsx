6050094f3a3c65004dab31f5b4b122f4a3afaa7e
javascriptsucks <evix1988@live.cn>
2023-10-14 02:07:44 -0600

    implement structure of cards and install shadcn card component ui

--- src/components/DemoCards/DemoCards.tsx
+++ src/components/DemoCards/DemoCards.tsx
@@ -4,11 +4,13 @@ import CardItem from '@components/DemoCards/CardItem.tsx/CardItem'
 function DemoCards() {
   return (
     <div className="absolute top-60 w-full space-y-10 py-2.5 text-center text-xs leading-4 text-white md:top-40">
-      <section className="flex gap-10">
-        Works
-        <CardItem />
-        <CardItem />
-        <CardItem />
+      <section className="px-20">
+        <title className="block text-left">How It Works</title>
+        <div className="flex justify-evenly gap-10">
+          <CardItem />
+          <CardItem />
+          <CardItem />
+        </div>
       </section>
       <section className="flex gap-10">
         Benefits
