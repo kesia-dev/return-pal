b444e546e0298b7625264b29a43c009de22fe042
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-15 19:53:44 -0400

    Migrate MobileMenuFooter icons to FontAwesome

--- src/components/Header/MobileMenuFooter.tsx
+++ src/components/Header/MobileMenuFooter.tsx
@@ -1,26 +1,46 @@
+import {
+  faInstagram,
+  faLinkedin,
+  faXTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 
-function MobileMenuFooter() {
+export default function MobileMenuFooter() {
   return (
     <div className="mt-12 flex w-[70%] justify-around rounded-md border border-primary p-2 text-primary">
       <Link href="/" target="_blank">
-        <FaInstagram size={25} />
+        <FontAwesomeIcon
+          icon={faInstagram}
+          width={'25'}
+          className="text-primary"
+        />
       </Link>
       <Link
         href="https://www.linkedin.com/company/return-pal/about/"
         target="_blank"
       >
-        <FaLinkedin size={25} />
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          width={'25'}
+          className="text-primary"
+        />
       </Link>
       <Link href="/" target="_blank">
-        <FaTwitter size={25} />
+        <FontAwesomeIcon
+          icon={faXTwitter}
+          width={'25'}
+          className="text-primary"
+        />
       </Link>
       <Link href="/" target="_blank">
-        <FaYoutube size={25} />
+        <FontAwesomeIcon
+          icon={faYoutube}
+          width={'25'}
+          className="text-primary"
+        />
       </Link>
     </div>
   )
 }
-
-export default MobileMenuFooter
