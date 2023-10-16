4338e92d368f6805f239a575789e15892ca9a2d9
RJ <randy.j416@yahoo.com>
2023-10-16 18:29:39 -0400

    modify css to make it more aligned on mobile devices

--- src/pages/address.tsx
+++ src/pages/address.tsx
@@ -71,6 +71,10 @@ function Address() {
       console.log("adding new address to profile!");
     } catch (err) {
       console.log(err);
+      toast({
+        variant: "destructive",
+        description: "Please ensure that all fields are filled in and province field contains only two letters",
+      })
     }
   };
   useEffect(() => {
@@ -113,12 +117,12 @@ function Address() {
   }
   const addressArrayMapper = (addressArray: { name: string, address: string, default: boolean }[]) => {
     return addressArray.map((address) => {
-      return (<div className="w-full h-10 flex my-5 fontSize-large items-center"><Input type="radio" key={addressArray.indexOf(address)} value={address.address} name="address" className="h-6 w-8 mx-5"/> <label className="font-bold mx-2">
-        {address.name} </label> <label> {address.address} </label> <label className="text-primary font-bold mx-2">
+      return (<div className="w-full h-10 flex my-7 fontSize-large items-center "><Input type="radio" key={addressArray.indexOf(address)} value={address.address} name="address" className="h-6 w-8 mx-2 w-[10%]"/> <Label className="font-bold mx-2 w-[10%] break-all">
+        {address.name} </Label> <Label className="w-[50%] my-2 py-2 break-all max-w-max"> {address.address} </Label> <Label className="text-primary font-bold mx-2 w-[10%]">
           {address.default && (
             "Default address"
           )}
-        </label>
+        </Label>
       </div>)
     })
   }
@@ -147,19 +151,19 @@ function Address() {
             <div className="items-center flex flex-row ">
             <div className="mr-5 items-center">
               <div className="my-2 flex items-center" >
-                <Label className="w-1/4">Name:</Label>
+                <Label className="w-1/3">Name:</Label>
                 <Input type="text" name="name" className="my-2  w-3/4" />
               </div>
               <div className="my-2 flex items-center">
-                <Label className="w-1/4">Address: </Label>
+                <Label className="w-1/3">Address: </Label>
                 <Input type="text" name="address" className="my-2  w-3/4" />
               </div>
               <div className="my-2 flex items-center">
-                <Label className="w-1/4">City: </Label>
+                <Label className="w-1/3">City: </Label>
                 <Input type="text" name="address" className="my-2 w-3/4" />
               </div>
               <div className="my-2 flex items-center">
-                <Label className="w-1/4">Province: (two letters) </Label>
+                <Label className="w-1/3">Province: (e.g. ON) </Label>
                 <Input type="text" name="address" className="my-2  w-3/4" />
               </div>
             </div>
