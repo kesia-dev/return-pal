6886d290622f2b4d80b33b1c0f2f1898b28f6c0b
RJ <randy.j416@yahoo.com>
2023-10-17 14:15:09 -0400

    exchange different svg for handoff; add logic to immediately add a valid address to selection

--- src/pages/address.tsx
+++ src/pages/address.tsx
@@ -10,7 +10,7 @@ import Router from 'next/router';
 import { useToast } from '@/components/ui/use-toast';
 
 function Address() {
-  const [addresses, setAddresses] = useState({});
+  const [addresses, setAddresses] = useState<{ name: string, address: string, default: boolean }[]>([]);
   const [step, setStep] = useState(1);
   const [addAddress, setAddAddress] = useState(false);
   const { toast } = useToast()
@@ -53,28 +53,36 @@ function Address() {
     }
   ]
   const addressSchema = z.object({
+    name: z.string().min(2),
     address: z.string().min(3).max(50),
     city: z.string().min(3).max(50),
     province: z.string().min(2).max(2),
-  })
+    postal: z.string().min(6),
+  });
   const validateFormData = (inputs: unknown) => {
     const isValidData = addressSchema.parse(inputs);
     return isValidData;
   };
-  const addressValidator = async (addressObj: object) => {
+  const addressValidator = async (addressObj: { name: string, address: string, city: string, province: string, postal: string }) => {
     try {
       const valid = validateFormData(addressObj);
-      if(!valid) {
+      if (!valid) {
         return;
       }
       console.log(valid);
       console.log("adding new address to profile!");
+      const newAddress: { name: string, address: string, default: boolean } = {
+        name: addressObj.name,
+        address: `${addressObj.address}, ${addressObj.city}, ${addressObj.province}, ${addressObj.postal}`,
+        default: false,
+      };
+      setAddresses([...addresses, newAddress]);
     } catch (err) {
       console.log(err);
       toast({
         variant: "destructive",
         description: "Please ensure that all fields are filled in and province field contains only two letters",
-      })
+      });
     }
   };
   useEffect(() => {
@@ -82,11 +90,13 @@ function Address() {
   }, [])
   const addNewAddress = (event: any) => {
     event.preventDefault();
-    const addressToAdd: object = {
+    const addressToAdd: { name: string, address: string, city: string, province: string, postal: string } = {
+      name: event.target[0].value,
       address: event.target[1].value,
       city: event.target[2].value,
       province: event.target[3].value,
-    } 
+      postal: event.target[4].value,
+    };
     addressValidator(addressToAdd);
   }
   const toggleAddressForm = () => {
@@ -106,18 +116,18 @@ function Address() {
       console.log(selectedValue);
 
       if (selectedValue) {
-        Router.push("/pickup")
+        Router.push("/pickup");
       } else {
         toast({
           variant: "destructive",
           description: "Please select an address before proceeding.",
-        })
+        });
       }
     }
   }
   const addressArrayMapper = (addressArray: { name: string, address: string, default: boolean }[]) => {
     return addressArray.map((address) => {
-      return (<div className="w-full h-10 flex my-7 fontSize-large items-center "><Input type="radio" key={addressArray.indexOf(address)} value={address.address} name="address" className="h-6 w-8 mx-2 w-[10%]"/> <Label className="font-bold mx-2  w-[20%] sm:w-[10%] break-word ">
+      return (<div className="w-full h-10 flex my-7 fontSize-large items-center "><Input type="radio" key={addressArray.indexOf(address)} value={address.address} name="address" className="h-6 w-8 mx-2 w-[10%]" /> <Label className="font-bold mx-2  w-[20%] sm:w-[10%] break-word ">
         {address.name} </Label> <Label className="w-[35%] sm:w-[50%] my-2 py-2 break-word max-w-max "> {address.address} </Label> <Label className="text-primary font-bold mx-2 w-[10%]">
           {address.default && (
             "Default address"
@@ -142,35 +152,35 @@ function Address() {
         <Separator className="w-3/4 border-black border-t-2" />
         <div>
           <form id="selectAddressForm" className="mt-5">
-            {addressArrayMapper(mockAddresses)}
+            {addresses.length && addressArrayMapper(addresses)}
           </form>
         </div>
         <Button className="bg-transparent hover:bg-transparent text-primary font-bold" onClick={toggleAddressForm}>+ Add a new address </Button>
         {addAddress && (<form className="flex flex-column justify-around w-3/4" onSubmit={(e) => addNewAddress(e)}>
           <div className="flex flex-column">
             <div className="items-center flex flex-row ">
-            <div className="mr-5 items-center">
-              <div className="my-2 flex items-center" >
-                <Label className="w-1/3">Name:</Label>
-                <Input type="text" name="name" className="my-2  w-3/4" />
+              <div className="mr-5 items-center">
+                <div className="my-2 flex items-center" >
+                  <Label className="w-1/3">Name:</Label>
+                  <Input type="text" name="name" className="my-2  w-3/4" />
+                </div>
+                <div className="my-2 flex items-center">
+                  <Label className="w-1/3">Address: </Label>
+                  <Input type="text" name="address" className="my-2  w-3/4" />
+                </div>
+                <div className="my-2 flex items-center">
+                  <Label className="w-1/3">City: </Label>
+                  <Input type="text" name="address" className="my-2 w-3/4" />
+                </div>
+                <div className="my-2 flex items-center">
+                  <Label className="w-1/3">Province: (e.g. ON) </Label>
+                  <Input type="text" name="address" className="my-2  w-3/4" />
+                </div>
+                <div className="my-2 flex items-center">
+                  <Label className="w-1/3">Postal Code </Label>
+                  <Input type="text" name="address" className="my-2  w-3/4" />
+                </div>
               </div>
-              <div className="my-2 flex items-center">
-                <Label className="w-1/3">Address: </Label>
-                <Input type="text" name="address" className="my-2  w-3/4" />
-              </div>
-              <div className="my-2 flex items-center">
-                <Label className="w-1/3">City: </Label>
-                <Input type="text" name="address" className="my-2 w-3/4" />
-              </div>
-              <div className="my-2 flex items-center">
-                <Label className="w-1/3">Province: (e.g. ON) </Label>
-                <Input type="text" name="address" className="my-2  w-3/4" />
-              </div>
-            </div>
-            <div>
-              
-              
-            </div>
             </div>
           </div>
           <div className="flex justify-around items-center">
