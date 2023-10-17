64a96a20f251871951e729d239648593b8b8b32d
mike-dalton <michael.j.dalton@gmail.com>
2023-10-17 10:26:43 -0400

    add navigation buttons below plan choices

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -42,27 +42,3 @@ function SignIn() {
 }
 
 export default SignIn
-
-{
-  /* <button
-        type="button"
-        onClick={() => router.back()}
-        className="absolute bottom-[40px] left-[13%] flex items-center bg-transparent text-base font-normal text-primary sm:bottom-[80px] sm:left-[10%] sm:text-xl"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-4 w-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-          />
-        </svg>
-        <span>&nbsp;Back</span>
-      </button> */
-}
