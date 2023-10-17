40b0d74ad9a9900cd8eda390a501254bf68efb47
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-17 12:55:22 -0400

    Added home icon to 'Back to Home' button and made page mobile friendly

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { getLayout } from '@/layouts/DefaultLayout'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Head from 'next/head'
 import Link from 'next/link'
 
@@ -12,13 +14,19 @@ export default function NotFoundPage() {
       </Head>
 
       <div className="flex h-screen flex-col items-center justify-center space-y-10">
-        <p className="text-9xl font-light text-primary">404</p>
-        <div className="flex flex-col">
+        <p className="text-9xl text-primary">404</p>
+        <div className="container flex flex-col items-center">
           <div className="mb-6 text-brand">
-            The requested page you are looking for doesn&rsquo;t exist!
+            The requested page you are looking for doesn&rsquo;t exist...
           </div>
           <Button asChild>
-            <Link href="/">Back To Home</Link>
+            <Link
+              className="flex cursor-pointer items-center space-x-2"
+              href="/"
+            >
+              <p>Back To Home</p>
+              <FontAwesomeIcon width={'15'} height={'15'} icon={faHome} />
+            </Link>
           </Button>
         </div>
       </div>
