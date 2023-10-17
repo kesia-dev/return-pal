4ea8ecd344c6c575dc6b2f61df03ea64e669ecbf
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-17 12:14:50 -0400

    Update Header component to be more resposinve by removing the title and show the logo only depending on the size of the screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import { HeaderRoot, HeaderSub } from '@components/Headers/Header'
 import MobileMenu from '@components/Headers/MobileHeader'
 import DesktopHeader from '@components/Headers/DesktopHeader'
+
+import Logo from '@components/icon/Logo'
+import ReturnPalTitle from '@components/icon/ReturnPalTitle'
 
 export default function Header() {
   return (
     <HeaderRoot>
       <HeaderSub>
         <Link href="/">
-          <Image src={'/navbar-logo.png'} alt="logo" width={200} height={200} />
+          <div className="flex items-center space-x-2">
+            <ReturnPalTitle className="hidden h-10 w-40 lg:flex" />
+            <Logo className="h-7 w-7 shrink-0 fill-primary" />
+          </div>
         </Link>
         <MobileMenu />
         <DesktopHeader />
