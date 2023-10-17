de33d9aed93f7dedf265aebc7e17149990555825
RJ <randy.j416@yahoo.com>
2023-10-17 15:14:01 -0400

    initial commit

---
+++ src/pages/pickup-info.tsx
@@ -0,0 +1,11 @@
+import { getLayout } from '@/layouts/DefaultLayout'
+
+export default function PickupInfo () {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+PickupInfo.getLayout = getLayout
