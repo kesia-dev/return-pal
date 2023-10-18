541ccdbf2815f5f2848abb6da4f7d15972f8afec
RJ <randy.j416@yahoo.com>
2023-10-18 10:28:18 -0400

    stash before working on different branch

--- src/pages/pickup-info.tsx
+++ src/pages/pickup-info.tsx
@@ -1,10 +1,10 @@
 import { getLayout } from '@/layouts/DefaultLayout'
 
-export default function PickupInfo () {
+export default function PickupInfo() {
   return (
     <div className="flex flex-col flex-wrap justify-around">
-      <div className="flex flex-row justify-between mx-5">
-        <div className="min-w-fit w-2/5 border-4 rounded-lg border-primary bg-white">
+      <div className="mx-5 flex flex-row justify-between">
+        <div className="w-2/5 min-w-fit rounded-lg border-4 border-primary bg-white">
           <div className="">
             <div className="flex flex-row bg-paleBlue">
               <div>Attachment</div>
@@ -13,20 +13,17 @@ export default function PickupInfo () {
             </div>
             <div>money</div>
           </div>
-            
-
         </div>
-        <div className="flex flex-col md:flex-row w-3/5">
-          
-        <div className="min-w-fit w-[33%] border-4 rounded-md border-brand">
-          Physical Label
-        </div>
-        <div className="min-w-fit w-[33%] border-4 rounded-md border-brand">
-          Digital Label
-        </div>
-        <div className="min-w-fit w-[33%] border-4 rounded-md border-brand">
-          Amazon QR Code
-        </div>
+        <div className="flex w-3/5 flex-col md:flex-row">
+          <div className="w-[33%] min-w-fit rounded-md border-4 border-brand">
+            Physical Label
+          </div>
+          <div className="w-[33%] min-w-fit rounded-md border-4 border-brand">
+            Digital Label
+          </div>
+          <div className="w-[33%] min-w-fit rounded-md border-4 border-brand">
+            Amazon QR Code
+          </div>
         </div>
       </div>
     </div>
