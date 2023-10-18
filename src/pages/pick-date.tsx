caca3b78a2bab652e2b12bb2aa0307a09c39fa95
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-18 17:29:26 -0400

    Switch pick date page to return process layout

--- src/pages/pick-date.tsx
+++ src/pages/pick-date.tsx
@@ -1,5 +1,3 @@
-import { HeaderRoot, HeaderSub } from '@/components/Headers/Header'
-import ReturnPalTitle from '@/components/SvgComponents/ReturnPalTitle'
 import {
   HomeSection,
   SectionDescription,
@@ -9,7 +7,6 @@ import {
 import { Card, CardContent } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
 import {
-  faCheck,
   faChevronLeft,
   faChevronRight,
   faClose,
@@ -18,6 +15,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 import { type PropsWithChildren } from 'react'
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
+import { getLayout } from '@/layouts/ReturnProcessLayout'
+import Head from 'next/head'
 
 type PickCardType = PropsWithChildren & {
   dayNum: number
@@ -25,6 +24,10 @@ type PickCardType = PropsWithChildren & {
   selected?: boolean
 }
 
+// TODO: Selecting a card moves all the other cards down, make sure only the selected card grows and the other ones don't move
+
+// TODO: Convert PickDateCard into a forwardRef component
+// https://www.radix-ui.com/primitives/docs/guides/composition#your-component-must-forward-ref
 function PickDateCard({ dayNum, day, ...props }: PickCardType) {
   return (
     <Card
@@ -45,107 +48,9 @@ function PickDateCard({ dayNum, day, ...props }: PickCardType) {
 export default function PickDate() {
   return (
     <>
-      <HeaderRoot className="border-brand bg-brand">
-        <HeaderSub className="gap-x-12">
-          <Link href="/">
-            <div className="flex items-center space-x-2">
-              <ReturnPalTitle className="hidden h-10 w-40 fill-white lg:flex" />
-            </div>
-          </Link>
-
-          {/* <ol className="items-center justify-center sm:flex">
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                <div className="z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary">
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    width="15"
-                    height="15"
-                    className="text-white"
-                  />
-                </div>
-                <div className="hidden h-1 w-full bg-primary dark:bg-gray-700 sm:flex"></div>
-              </div>
-              <div className="abs mt-1 sm:pr-8">
-                <p className="text-sm font-normal text-white">Pickup Date</p>
-              </div>
-            </li>
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                <div className="z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full border-4 border-primary" />
-                <div className="hidden h-1 w-full bg-primary dark:bg-gray-700 sm:flex" />
-              </div>
-              <div className="abs mt-1 sm:pr-8">
-                <p className="text-sm font-normal text-white">Pickup Details</p>
-              </div>
-            </li>
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                <div className="z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full border-2 border-primary" />
-                <div className="hidden h-1 w-full bg-primary dark:bg-gray-700 sm:flex" />
-              </div>
-              <div className="abs mt-1 sm:pr-8">
-                <p className="text-sm font-normal text-white">Pickup Date</p>
-              </div>
-            </li>
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                <div className="z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full border-2 border-primary" />
-                <div className="hidden h-1 w-full bg-primary dark:bg-gray-700 sm:flex" />
-              </div>
-              <div className="abs mt-1 sm:pr-8">
-                <p className="text-sm font-normal text-white">Pickup Date</p>
-              </div>
-            </li>
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                <div className="z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full border-2 border-primary" />
-              </div>
-              <div className="abs mt-1 sm:pr-8">
-                <p className="text-sm font-normal text-white">Pickup Date</p>
-              </div>
-            </li>
-          </ol> */}
-
-          {/* <div className="w-full px-4">
-            <div className="mx-auto w-full max-w-md">
-              <div className="relative">
-                <div className="absolute left-0 top-1/2 -mt-px h-0.5 w-full bg-primary" />
-                <ul className="relative flex w-full justify-between">
-                  <li className="relative">
-                    <div className="flex h-7 w-7 items-center justify-center rounded-full border-4 border-primary bg-primary">
-                      <FontAwesomeIcon
-                        icon={faCheck}
-                        width="15"
-                        height="15"
-                        className="text-white"
-                      />
-                    </div>
-                    <div className="absolute left-[-1rem] right-0 m-auto w-screen text-sm text-white">
-                      Pickup Date
-                    </div>
-                  </li>
-                  <li className="relative">
-                    <div className="flex h-7 w-7 items-center justify-center rounded-full border-4 border-primary bg-brand" />
-                    <div className="absolute left-[-2rem]  right-0 m-auto w-screen text-sm font-bold text-white">
-                      Pickup Details
-                    </div>
-                  </li>
-                  <li>
-                    <div className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-primary bg-brand" />
-                  </li>
-                  <li>
-                    <div className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-primary bg-brand" />
-                  </li>
-                  <li>
-                    <div className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-primary bg-brand" />
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div> */}
-        </HeaderSub>
-      </HeaderRoot>
+      <Head>
+        <title>Pick Date</title>
+      </Head>
 
       <div className="container space-y-20 bg-paleBlue pt-16">
         <HomeSection className="items-start space-y-3">
@@ -254,4 +159,4 @@ export default function PickDate() {
   )
 }
 
-// PickDate.getLayout = getLayout
+PickDate.getLayout = getLayout
