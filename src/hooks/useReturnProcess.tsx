1c86dbd27fc14f4e11aeec68cfee73e034d38410
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-24 11:07:47 -0400

    Ensure back and forward methods can't go to an out-of-bounds step

--- src/hooks/useReturnProcess.tsx
+++ src/hooks/useReturnProcess.tsx
@@ -1,5 +1,5 @@
 import { ReturnProcessContext } from '@/context/ReturnProcessContext'
-import { useContext, useState } from 'react'
+import { useCallback, useContext, useMemo, useState } from 'react'
 
 export function useReturnProcess() {
   const context = useContext(ReturnProcessContext)
@@ -14,7 +14,41 @@ export function useReturnProcess() {
     throw new Error('useReturnProcess is supposed to have at least one step')
   }
 
-  const [currentStep, setCurrentStep] = useState(context.steps[0]!)
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
 
-  return { ...context, currentStep }
+  const getCurrentStep = useCallback(() => {
+    // console.log('getCurrentStep changed')
+    context.steps[currentStepIndex]
+  }, [context.steps, currentStepIndex])
+
+  const getCurrentMemo = useMemo(() => {
+    const t = context.steps[currentStepIndex]
+    // console.log('getCurrentMemo changed ', t)
+    return t
+  }, [context.steps, currentStepIndex])
+
+  function forward() {
+    if (currentStepIndex === context.steps.length - 1) {
+      return
+    }
+    setCurrentStepIndex(currentStepIndex + 1)
+  }
+
+  function back() {
+    if (currentStepIndex <= 0) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev - 1)
+  }
+
+  // console.log('currentStepIndex', currentStepIndex)
+
+  return {
+    steps: context.steps,
+    currentStepIndex,
+    getCurrentStep,
+    getCurrentMemo,
+    forward,
+    back,
+  }
 }
