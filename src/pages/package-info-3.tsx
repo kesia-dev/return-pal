378848027b1df9eef00b299b13670a446c0fad87
RJ <randy.j416@yahoo.com>
2023-10-24 17:46:46 -0400

    implement description editing and deleting package labels

--- src/pages/package-info-3.tsx
+++ src/pages/package-info-3.tsx
@@ -38,50 +38,6 @@ type FileUploadType = {
   description: string | null
 }
 
-export const columns: ColumnDef<FileUploadType>[] = [
-  {
-    accessorKey: 'attachment',
-    header: 'Attachment',
-    cell: ({ row }) => (
-      <div className="break-all text-center">{row.original.attachment}</div>
-    ),
-  },
-  {
-    accessorKey: 'labelType',
-    header: 'Label Type',
-    cell: ({ row }) => (
-      <Badge className="text-slate bg-green-200">
-        {row.original.labelType}
-      </Badge>
-    ),
-  },
-  {
-    accessorKey: 'description',
-    header: 'Description',
-  },
-  {
-    accessorKey: ' ',
-    cell: () => (
-      <div className="flex justify-end space-x-1">
-        <FontAwesomeIcon
-          className="text-primary"
-          onClick={() => console.log(123)}
-          icon={faPen}
-          width={'15'}
-          height={'15'}
-        />
-        <FontAwesomeIcon
-          className="text-primary"
-          onClick={() => console.log(456)}
-          icon={faTrashCan}
-          width={'15'}
-          height={'15'}
-        />
-      </div>
-    ),
-  },
-]
-
 const uploads: FileUploadType[] = [
   {
     attachment: 'INV001',
@@ -100,13 +56,136 @@ const uploads: FileUploadType[] = [
   },
 ]
 
+const uploadButtonClasses =
+  'flex w-[30%] min-w-[30%] max-w-2xl flex-col justify-between rounded-lg border-4 border-brand bg-white font-bold text-brand lg:text-2xl'
+
 export default function PackageInfo3() {
   const [arrayOfLabels, setArrayOfLabels] = useState<FileUploadType[]>([])
   const [labelDescription, setLabelDescription] = useState<string | null>(null)
   const [file, setFile] = useState<File | null>(null)
+  const columns: ColumnDef<FileUploadType>[] = [
+    {
+      accessorKey: 'attachment',
+      header: 'Attachment',
+      cell: ({ row }) => (
+        <div className="break-all text-center">{row.original.attachment}</div>
+      ),
+    },
+    {
+      accessorKey: 'labelType',
+      header: 'Label Type',
+      cell: ({ row }) => (
+        <Badge className="text-slate bg-green-200">
+          {row.original.labelType}
+        </Badge>
+      ),
+    },
+    {
+      accessorKey: 'description',
+      header: 'Description',
+    },
+    {
+      accessorKey: ' ',
+      cell: (row) => {
+        let newDescription: string
+        return (
+          <div className="flex justify-end space-x-1">
+            <Dialog>
+              <DialogTrigger>
+                <FontAwesomeIcon
+                  className="text-primary"
+                  icon={faPen}
+                  width={'15'}
+                  height={'15'}
+                />
+              </DialogTrigger>
+              <DialogContent className="bg-paleBlue">
+                <DialogHeader>
+                  <DialogTitle className="text-center font-bold text-brand">
+                    Edit Label Description
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="font-bold text-brand">
+                  <Label htmlFor="description" className="text-right">
+                    Description
+                  </Label>
+                  <Input
+                    id="description"
+                    onChange={(e) => {
+                      newDescription = e.target.value
+                    }}
+                    className="col-span-3"
+                  />
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button
+                      className="w-full px-5"
+                      onClick={() => {
+                        const newArrayOfLabels = [...arrayOfLabels]
+                        const rowId = Number(row.row.id)
+
+                        if (newArrayOfLabels[rowId]) {
+                          newArrayOfLabels[rowId]!.description = newDescription
+                        }
+                        setArrayOfLabels(newArrayOfLabels)
+                        console.log('saving description')
+                      }}
+                    >
+                      {' '}
+                      Save Package Info
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+
+            <Dialog>
+              <DialogTrigger>
+                <FontAwesomeIcon
+                  className="text-primary"
+                  onClick={() => console.log(456)}
+                  icon={faTrashCan}
+                  width={'15'}
+                  height={'15'}
+                />
+              </DialogTrigger>
+              <DialogContent className="bg-paleBlue">
+                <DialogHeader>
+                  <DialogTitle className="text-center font-bold text-brand">
+                    Delete Label
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="font-bold text-brand">
+                  This action cannot be undone. Are you sure you want to
+                  permanently delete this file from our servers?
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button
+                      type="submit"
+                      className="w-full px-5"
+                      onClick={() => {
+                        const newArrayOfLabels = [...arrayOfLabels]
+                        const rowId = Number(row.row.id)
+                        newArrayOfLabels.splice(rowId, 1)
+                        setArrayOfLabels(newArrayOfLabels)
+                      }}
+                    >
+                      {' '}
+                      Delete
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
+        )
+      },
+    },
+  ]
   const handleChange = (loadedFile: File) => {
     setFile(loadedFile)
-    console.log(file)
     console.log(loadedFile)
   }
   const table = useReactTable({
@@ -270,10 +349,8 @@ export default function PackageInfo3() {
         </div>
 
         <div className="my-5 flex flex-row justify-between px-5 text-center lg:my-0 lg:w-1/2">
-          {/* <div className="flex w-[30%] min-w-[30%] flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
-          </div> */}
           <Dialog>
-            <DialogTrigger className="flex w-[30%] min-w-[30%] max-w-2xl flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
+            <DialogTrigger className={uploadButtonClasses}>
               <div className="mx-2 flex h-full grow flex-col justify-center self-center">
                 <div className="h-3/4">
                   <div className="mt-2 flex justify-center text-center">
@@ -300,14 +377,6 @@ export default function PackageInfo3() {
               <div className="flex flex-col px-5">
                 <div className="font-bold text-brand">Instructions</div>
                 <div className=" text-brand">
-                  {/* <Label htmlFor="description" className="text-right">
-                    Description
-                  </Label>
-                  <Input
-                    id="description"
-                    defaultValue='Label the item(s) inside: e.g. "laptop covers/"'
-                    className="col-span-3"
-                  /> */}
                   Please leave your physical label with your package.
                 </div>
               </div>
@@ -318,10 +387,8 @@ export default function PackageInfo3() {
               </DialogFooter>
             </DialogContent>
           </Dialog>
-          {/* <div className="flex w-[30%] min-w-[30%] flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
-          </div> */}
           <Dialog>
-            <DialogTrigger className="flex w-[30%] min-w-[30%] max-w-2xl  flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
+            <DialogTrigger className={uploadButtonClasses}>
               <div className="mx-2 flex h-full grow flex-col justify-center self-center">
                 <div className="h-3/4">
                   <div className="mt-2 flex justify-center text-center">
@@ -357,16 +424,35 @@ export default function PackageInfo3() {
                     types={fileTypes}
                   >
                     {' '}
-                    <p className=" text-gray-500">
-                      Drag label here or
-                      <Input type="file" id="files" className="hidden" />
-                      <Label
-                        className="text-mediumText text-primary"
-                        htmlFor="files"
-                      >
-                        {' '}
-                        browse files
-                      </Label>
+                    <p className="flex h-20 flex-col justify-around text-gray-500">
+                      <div className="flex justify-center">
+                        <svg
+                          width="50px"
+                          height="50px"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M12 10V16M12 10L10 12M12 10L14 12M12.0627 6.06274L11.9373 5.93726C11.5914 5.59135 11.4184 5.4184 11.2166 5.29472C11.0376 5.18506 10.8425 5.10425 10.6385 5.05526C10.4083 5 10.1637 5 9.67452 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V10.2C21 9.0799 21 8.51984 20.782 8.09202C20.5903 7.71569 20.2843 7.40973 19.908 7.21799C19.4802 7 18.9201 7 17.8 7H14.3255C13.8363 7 13.5917 7 13.3615 6.94474C13.1575 6.89575 12.9624 6.81494 12.7834 6.70528C12.5816 6.5816 12.4086 6.40865 12.0627 6.06274Z"
+                            stroke="#008BE7"
+                            strokeWidth="1"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                      <div className="self-center">
+                        Drag label here or
+                        <Input type="file" id="files" className="hidden" />
+                        <Label
+                          className="text-mediumText text-primary"
+                          htmlFor="files"
+                        >
+                          {' '}
+                          browse files
+                        </Label>
+                      </div>
                     </p>
                   </FileUploader>
                 </div>
@@ -377,7 +463,6 @@ export default function PackageInfo3() {
                   <Input
                     id="description"
                     defaultValue='Label the item(s) inside: e.g. "laptop covers/"'
-                    // value={labelDescription}
                     onChange={handleDescriptionChange}
                     className="col-span-3"
                   />
@@ -396,11 +481,8 @@ export default function PackageInfo3() {
               </DialogFooter>
             </DialogContent>
           </Dialog>
-          {/* <div className="flex w-[30%] min-w-[30%] flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
-            
-          </div> */}
           <Dialog>
-            <DialogTrigger className="flex w-[30%] min-w-[30%] max-w-2xl  flex-col justify-between rounded-md border-4 border-brand bg-white font-bold text-brand lg:text-2xl">
+            <DialogTrigger className={uploadButtonClasses}>
               <div className="mx-2 flex h-full grow flex-col justify-center self-center">
                 <div className="h-3/4">
                   <div className="mt-2 flex justify-center text-center">
@@ -428,21 +510,41 @@ export default function PackageInfo3() {
                 <div className="font-bold text-brand">Upload Return Label</div>
                 <div className="align-center min-h-20 flex h-20 flex-col justify-center rounded-lg border-2 bg-blue-200 text-center">
                   <FileUploader
+                    className="flex-grow"
                     handleChange={handleChange}
                     name="file"
                     types={fileTypes}
                   >
                     {' '}
-                    <p className=" text-gray-500">
-                      Drag label here or
-                      <Input type="file" id="files" className="hidden" />
-                      <Label
-                        className="text-mediumText text-primary"
-                        htmlFor="files"
-                      >
-                        {' '}
-                        browse files
-                      </Label>
+                    <p className="flex h-20 flex-col justify-around text-gray-500">
+                      <div className="flex justify-center">
+                        <svg
+                          width="50px"
+                          height="50px"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M12 10V16M12 10L10 12M12 10L14 12M12.0627 6.06274L11.9373 5.93726C11.5914 5.59135 11.4184 5.4184 11.2166 5.29472C11.0376 5.18506 10.8425 5.10425 10.6385 5.05526C10.4083 5 10.1637 5 9.67452 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V10.2C21 9.0799 21 8.51984 20.782 8.09202C20.5903 7.71569 20.2843 7.40973 19.908 7.21799C19.4802 7 18.9201 7 17.8 7H14.3255C13.8363 7 13.5917 7 13.3615 6.94474C13.1575 6.89575 12.9624 6.81494 12.7834 6.70528C12.5816 6.5816 12.4086 6.40865 12.0627 6.06274Z"
+                            stroke="#008BE7"
+                            strokeWidth="1"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                      <div className="self-center">
+                        Drag label here or
+                        <Input type="file" id="files" className="hidden" />
+                        <Label
+                          className="text-mediumText text-primary"
+                          htmlFor="files"
+                        >
+                          {' '}
+                          browse files
+                        </Label>
+                      </div>
                     </p>
                   </FileUploader>
                 </div>
@@ -453,7 +555,6 @@ export default function PackageInfo3() {
                   <Input
                     id="description"
                     defaultValue='Label the item(s) inside: e.g. "laptop covers/"'
-                    // value={labelDescription}
                     onChange={handleDescriptionChange}
                     className="col-span-3"
                   />
