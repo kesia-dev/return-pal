e8417cd6d29639088ec57513f72d61fe797d6f3c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-23 21:45:59 -0400

    Initial work on multi-step return process form

---
+++ src/pages/context-test.tsx
@@ -0,0 +1,25 @@
+import { getLayout } from '@/layouts/ReturnProcessLayout'
+import PickDate from './pick-date'
+import ReturnProcessContextProvider from '@/context/ReturnProcessContext'
+import Pickup from './pickup'
+
+const steps = [
+  {
+    id: 'pick-date',
+    component: <PickDate />,
+  },
+  {
+    id: 'pickup-details',
+    component: <Pickup />,
+  },
+]
+
+export default function ContextTest() {
+  return (
+    <ReturnProcessContextProvider>
+      <div>asd</div>
+    </ReturnProcessContextProvider>
+  )
+}
+
+ContextTest.getLayout = getLayout
