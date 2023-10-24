051cf0878117b1a6a2e9116a41ca0c1179f7c7c6
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-24 18:57:41 -0400

    Initial work done on pick-date page to use multi-step form (currently only validates selection)

--- src/test_pages/pick-date.tsx
+++ src/test_pages/pick-date.tsx
@@ -11,13 +11,25 @@ import {
   faChevronRight,
   faClose,
 } from '@fortawesome/free-solid-svg-icons'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import Head from 'next/head'
 import { ReturnProcessNextButton } from '@/components/ui/common'
 import { useReturnProcess } from '@/hooks/useReturnProcess'
+import { Button } from '@/components/ui/button'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 // TODO: Change type to just accept a full date instead of having 2 props for day number and actual name date
 type PickCardType = React.HTMLAttributes<HTMLDivElement> & {
@@ -41,7 +53,7 @@ const PickDateCard = React.forwardRef<HTMLDivElement, PickCardType>(
         {...props}
       >
         <CardContent className="flex flex-col items-center space-y-4 pt-6">
-          <p className="text-2xl font-semibold">Sep</p>
+          <p className="text-2xl font-semibold">Oct</p>
           <p className="text-5xl font-bold">{dayNum}</p>
           <p className="text-2xl font-semibold">{day}</p>
         </CardContent>
@@ -53,6 +65,35 @@ PickDateCard.displayName = 'PickDateCard'
 
 export default function PickDate() {
   const returnProcess = useReturnProcess()
+  const [initialDate] = useState(new Date())
+  const formSchema = z.object({
+    pickupDate: z.string().min(2),
+    // pickupDate: z.coerce.date({
+    //   invalid_type_error: 'Must be a valid date',
+    //   required_error: 'Date selection is required',
+    // }),
+    // .min(initialDate, {
+    //   message: 'You cannot pick a date in the past',
+    // }),
+  })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      pickupDate: '',
+    },
+  })
+  const { isDirty, isValid } = form.formState
+  console.log('form.formState.isValid): ', form.formState.isValid)
+  console.log('form.formState.error: ', form.formState.errors)
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log('Submitted:', values)
+  }
+
+  // console.log('FORM pickupDate: ', form.getValues('pickupDate'))
+  // console.log('form.formState.isValid: ', form.formState.isValid)
+
   return (
     <>
       <Head>
@@ -106,7 +147,7 @@ export default function PickDate() {
           </div>
 
           <div className="flex">
-            <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
+            {/* <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
               <FontAwesomeIcon
                 size="2x"
                 width={'30'}
@@ -114,56 +155,81 @@ export default function PickDate() {
                 icon={faChevronLeft}
               />
               <p className="text-xl">Last Week</p>
-            </div>
-            <ToggleGroup.Root
-              type="single"
-              className="grid w-full grid-cols-1 place-content-center place-items-center content-center items-center justify-center gap-x-7 gap-y-5 xs:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7"
-              onValueChange={(s) => {
-                console.log('New value: ', s)
-              }}
-            >
-              <ToggleGroup.Item value="fri-22" asChild>
-                <PickDateCard dayNum={22} day="Fri" />
-              </ToggleGroup.Item>
+            </div> */}
+            <Form {...form}>
+              <form
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="pickupDate"
+                  render={({ field }) => (
+                    <FormItem className="space-y-3">
+                      <FormLabel>Notify me about...</FormLabel>
+                      <FormControl>
+                        <ToggleGroup.Root
+                          type="single"
+                          className="grid w-full grid-cols-1 place-content-center place-items-center content-center items-center justify-center gap-x-7 gap-y-5 xs:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7"
+                          // onValueChange={(s) => {
+                          //   // form.setValue('pickupDate', new Date(s))
+                          //   form.setValue('pickupDate', s)
+                          //   console.log('New value: ', s)
+                          // }}
+                          // value={form.getValues('pickupDate')}
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <ToggleGroup.Item value="2023-10-22" asChild>
+                            <PickDateCard dayNum={22} day="Fri" />
+                          </ToggleGroup.Item>
 
-              <ToggleGroup.Item value="sat-23" asChild>
-                <PickDateCard dayNum={23} day="Sat" />
-              </ToggleGroup.Item>
+                          <ToggleGroup.Item value="2023-10-23" asChild>
+                            <PickDateCard dayNum={23} day="Sat" />
+                          </ToggleGroup.Item>
 
-              <ToggleGroup.Item value="sun-24" asChild>
-                <PickDateCard dayNum={24} day="Sun" />
-              </ToggleGroup.Item>
-
-              <ToggleGroup.Item value="mon-25" asChild>
-                <PickDateCard dayNum={25} day="Mon" />
-              </ToggleGroup.Item>
-
-              <ToggleGroup.Item value="tue-26" asChild>
-                <PickDateCard dayNum={26} day="Tue" />
-              </ToggleGroup.Item>
-
-              <ToggleGroup.Item value="wed-27" asChild>
-                <PickDateCard dayNum={27} day="Wed" />
-              </ToggleGroup.Item>
-
-              <ToggleGroup.Item value="thu-28" asChild>
-                <PickDateCard dayNum={28} day="Thu" />
-              </ToggleGroup.Item>
-            </ToggleGroup.Root>
-            <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
-              <FontAwesomeIcon
-                size="2x"
-                width={'30'}
-                height={'45'}
-                icon={faChevronRight}
-              />
-              <p className="text-xl">Last Week</p>
-            </div>
+                          <ToggleGroup.Item value="2023-10-24" asChild>
+                            <PickDateCard dayNum={24} day="Sun" />
+                          </ToggleGroup.Item>
+                        </ToggleGroup.Root>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* 
+                <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
+                  <FontAwesomeIcon
+                    size="2x"
+                    width={'30'}
+                    height={'45'}
+                    icon={faChevronRight}
+                  />
+                  <p className="text-xl">Last Week</p>
+                </div> */}
+                <div>
+                  <Button type="submit">Submit</Button>
+                </div>
+              </form>
+            </Form>
           </div>
         </div>
 
         <div className="flex w-full flex-row-reverse">
-          <ReturnProcessNextButton onClick={() => returnProcess.forward()} />
+          <Button
+            variant={'default'}
+            disabled={!isDirty || !isValid}
+            onClick={() => returnProcess.forward()}
+          >
+            NEXT
+          </Button>
+          {/* <ReturnProcessNextButton
+            disabled={!isDirty || !isValid}
+            //onClick={() => returnProcess.forward()}
+          >
+            NEXT
+          </ReturnProcessNextButton> */}
         </div>
       </div>
     </>
