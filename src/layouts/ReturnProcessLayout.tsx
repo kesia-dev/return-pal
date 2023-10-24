141c5e88fc02e983f2da9065bf43557cdca87cc0
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-23 22:46:42 -0400

    Refactor context-test page and ReturnProcessLayout, and create temp pages to test multi step form

--- src/layouts/ReturnProcessLayout.tsx
+++ src/layouts/ReturnProcessLayout.tsx
@@ -1,11 +1,24 @@
 import { type PropsWithChildren } from 'react'
 import ReturnProcessHeader from '@/components/Headers/ReturnProcess/ReturnProcessHeader'
 import ReturnProcessContextProvider from '@/context/ReturnProcessContext'
+import PickDateTESTPAGE from '@/test_pages/pick-date_test'
+import PickupTESTPAGE from '@/test_pages/pickup_test'
+
+const steps = [
+  {
+    id: 'pick-date',
+    component: <PickDateTESTPAGE />,
+  },
+  {
+    id: 'pickup-details',
+    component: <PickupTESTPAGE />,
+  },
+]
 
 export default function ReturnProcessLayout({ children }: PropsWithChildren) {
   return (
-    <ReturnProcessContextProvider>
-      <div className={`flex h-[100dvh] overflow-hidden bg-paleBlue`}>
+    <ReturnProcessContextProvider steps={steps}>
+      <div className="flex h-[100dvh] overflow-hidden bg-paleBlue">
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <ReturnProcessHeader />
           <main className="grow">{children}</main>
--- src/pages/context-test.tsx
+++ src/pages/context-test.tsx
@@ -1,25 +1,7 @@
 import { getLayout } from '@/layouts/ReturnProcessLayout'
-import PickDate from './pick-date'
-import ReturnProcessContextProvider from '@/context/ReturnProcessContext'
-import Pickup from './pickup'
-
-const steps = [
-  {
-    id: 'pick-date',
-    component: <PickDate />,
-  },
-  {
-    id: 'pickup-details',
-    component: <Pickup />,
-  },
-]
 
 export default function ContextTest() {
-  return (
-    <ReturnProcessContextProvider steps={steps}>
-      <div>asd</div>
-    </ReturnProcessContextProvider>
-  )
+  return <div>asd</div>
 }
 
 ContextTest.getLayout = getLayout
