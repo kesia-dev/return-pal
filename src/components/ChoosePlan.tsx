f1fd51ac23778a2fc9d52b6e389c0f0591d7048e
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-25 15:01:16 -0400

    Add form wrapper to choose plan page

--- src/components/ChoosePlan.tsx
+++ src/components/ChoosePlan.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import Plan from './Plan'
 import { RadioGroup } from '@/components/ui/radio-group'
-import Link from 'next/link'
 import { ReturnProcessBackButton, ReturnProcessNextButton } from './ui/common'
 import { useReturnProcess } from '@/hooks/useReturnProcess'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form } from './ui/form'
 
 export type PlanDataType = {
   name: string
@@ -54,34 +57,65 @@ const planData: PlanDataType[] = [
   },
 ]
 
+const formSchema = z.object({
+  plan: z.union([
+    z.literal('bronze'),
+    z.literal('silver'),
+    z.literal('gold'),
+    z.literal('platinum'),
+  ]),
+})
+
 export default function ChoosePlan() {
   const returnProcess = useReturnProcess()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      plan: returnProcess.currentData.plan,
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log('Submitted:', values)
+    returnProcess.setCurrentData(values)
+    // returnProcess.forward()
+  }
+
   const [selectedPlanName, setSelectedPlanName] = useState('')
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-start bg-paleBlue p-10">
-      <RadioGroup className="flex w-full flex-wrap justify-center gap-8">
-        {planData.map((plan) => {
-          const status = !selectedPlanName
-            ? 'normal'
-            : selectedPlanName === plan.name
-            ? 'selected'
-            : 'unselected'
-          return (
-            <Plan
-              key={plan.name}
-              plan={plan}
-              status={status}
-              setSelectedPlanName={setSelectedPlanName}
-            />
-          )
-        })}
-      </RadioGroup>
-      <div className="mt-8 flex w-10/12 items-center justify-between">
-        <ReturnProcessBackButton onClick={() => returnProcess.back()} />
+    <Form {...form}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <div className="flex min-h-screen w-screen flex-col items-center justify-start bg-paleBlue p-10">
+          <RadioGroup className="flex w-full flex-wrap justify-center gap-8">
+            {planData.map((plan) => {
+              const status = !selectedPlanName
+                ? 'normal'
+                : selectedPlanName === plan.name
+                ? 'selected'
+                : 'unselected'
+              return (
+                <Plan
+                  key={plan.name}
+                  plan={plan}
+                  status={status}
+                  setSelectedPlanName={setSelectedPlanName}
+                />
+              )
+            })}
+          </RadioGroup>
+          <div className="mt-8 flex w-10/12 items-center justify-between">
+            <ReturnProcessBackButton onClick={() => returnProcess.back()} />
 
-        <ReturnProcessNextButton onClick={() => returnProcess.forward()} />
-      </div>
-    </div>
+            <ReturnProcessNextButton onClick={() => returnProcess.forward()} />
+          </div>
+        </div>
+      </form>
+    </Form>
   )
 }
--- src/test_pages/pricing.tsx
+++ src/test_pages/pricing.tsx
@@ -1,9 +1,15 @@
 import ChoosePlan from '@/components/ChoosePlan'
+import Head from 'next/head'
 
 export default function Pricing() {
   return (
-    <div className="h-screen w-screen max-w-4xl">
-      <ChoosePlan />
-    </div>
+    <>
+      <Head>
+        <title>Return Process - Pricing</title>
+      </Head>
+      <div className="h-screen w-screen max-w-4xl">
+        <ChoosePlan />
+      </div>
+    </>
   )
 }
