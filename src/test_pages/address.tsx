683821b224547706f306ff8c9d69e09a557b56be
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-25 14:52:17 -0400

    Refactored addresses page to work with multi-step form

--- src/test_pages/address.tsx
+++ src/test_pages/address.tsx
@@ -7,46 +7,48 @@ import { Separator } from '@/components/ui/separator'
 import { Label } from '@/components/ui/label'
 import Router from 'next/router'
 import { useToast } from '@/components/ui/use-toast'
-import Link from 'next/link'
 import {
   ReturnProcessBackButton,
   ReturnProcessNextButton,
 } from '@/components/ui/common'
 import { useReturnProcess } from '@/hooks/useReturnProcess'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+
+const formSchema = z.object({
+  address: z.string().min(1),
+})
 
 export default function Address() {
   const returnProcess = useReturnProcess()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      address: returnProcess.currentData.address,
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log('Submitted:', values)
+    returnProcess.setCurrentData(values)
+    // returnProcess.forward()
+  }
   const [addresses, setAddresses] = useState<
     { name: string; address: string; default: boolean }[]
   >([])
   const [addAddress, setAddAddress] = useState(false)
   const { toast } = useToast()
 
-  // const handleRadioChange = (event: any) => {
-  //   setSelectedMethod(event.target.value);
-  //   const form = document.getElementById("selectPickupMethod") as HTMLElement;
-  //   if (form) {
-  //     const radioInputs = form.querySelectorAll('input[type="radio"]');
-  //     let selectedValue;
-
-  //     radioInputs.forEach(function (input: any) {
-  //       const parentDiv: HTMLElement = input.parentNode?.parentNode;
-  //       if (parentDiv) {
-  //         if (input.checked) {
-  //           parentDiv?.classList.remove("border-brand");
-  //           parentDiv?.classList.add("border-primary");
-  //           parentDiv?.classList.remove("opacity-50")
-  //         } else {
-  //           parentDiv?.classList.add("border-brand");
-  //           parentDiv?.classList.remove("border-primary");
-  //           parentDiv?.classList.add("opacity-50")
-  //         }
-  //       }
-  //     });
-  //     setStep(2);
-  //   }
-
-  // };
   const mockAddresses: { name: string; address: string; default: boolean }[] = [
     {
       name: 'Bob Gunderson',
@@ -185,103 +187,127 @@ export default function Address() {
   }
 
   return (
-    <div className="mx-5 my-5">
-      <div className="text-largeText text-brand">Pickup Details</div>
-      <div>
-        <div className="text-brand">Select or add your pickup address</div>
-        <div className="mt-5 text-smallText font-bold text-brand">
-          Your Addresses:
-        </div>
-        <Separator className="w-3/4 border-t-2 border-black" />
-        <div>
-          <form id="selectAddressForm" className="mt-5">
-            {addresses.length && addressArrayMapper(addresses)}
-          </form>
-        </div>
-        <Button
-          className="bg-transparent font-bold text-primary hover:bg-transparent"
-          onClick={toggleAddressForm}
-        >
-          + Add a new address{' '}
-        </Button>
-        {addAddress && (
-          <form
-            className="flex-column flex w-3/4 justify-around"
-            onSubmit={(e) => addNewAddress(e)}
-          >
-            <div className="flex-column flex">
-              <div className="flex flex-row items-center ">
-                <div className="mr-5 items-center">
-                  <div className="my-2 flex items-center">
-                    <Label className="w-1/3">Name:</Label>
-                    <Input type="text" name="name" className="my-2  w-3/4" />
-                  </div>
-                  <div className="my-2 flex items-center">
-                    <Label className="w-1/3">Address: </Label>
-                    <Input type="text" name="address" className="my-2  w-3/4" />
-                  </div>
-                  <div className="my-2 flex items-center">
-                    <Label className="w-1/3">City: </Label>
-                    <Input type="text" name="address" className="my-2 w-3/4" />
-                  </div>
-                  <div className="my-2 flex items-center">
-                    <Label className="w-1/3">Province: (e.g. ON) </Label>
-                    <Input type="text" name="address" className="my-2  w-3/4" />
-                  </div>
-                  <div className="my-2 flex items-center">
-                    <Label className="w-1/3">Postal Code </Label>
-                    <Input type="text" name="address" className="my-2  w-3/4" />
+    <Form {...form}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="mx-5 my-5">
+          <div className="text-largeText text-brand">Pickup Details</div>
+          <div>
+            <div className="text-brand">Select or add your pickup address</div>
+            <div className="mt-5 text-smallText font-bold text-brand">
+              Your Addresses:
+            </div>
+            <Separator className="h-[0.15rem] w-3/4 rounded-full bg-brand" />
+            <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      {mockAddresses.map((address) => {
+                        return (
+                          <FormItem
+                            key={address.address}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem value={address.address} />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {address.address}
+                            </FormLabel>
+                          </FormItem>
+                        )
+                      })}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                  <Button type="submit">Submit</Button>
+                </FormItem>
+              )}
+            />
+
+            <Button
+              className="bg-transparent font-bold text-primary hover:bg-transparent"
+              onClick={toggleAddressForm}
+            >
+              + Add a new address{' '}
+            </Button>
+            {addAddress && (
+              <form
+                className="flex-column flex w-3/4 justify-around"
+                onSubmit={(e) => addNewAddress(e)}
+              >
+                <div className="flex-column flex">
+                  <div className="flex flex-row items-center ">
+                    <div className="mr-5 items-center">
+                      <div className="my-2 flex items-center">
+                        <Label className="w-1/3">Name:</Label>
+                        <Input
+                          type="text"
+                          name="name"
+                          className="my-2  w-3/4"
+                        />
+                      </div>
+                      <div className="my-2 flex items-center">
+                        <Label className="w-1/3">Address: </Label>
+                        <Input
+                          type="text"
+                          name="address"
+                          className="my-2  w-3/4"
+                        />
+                      </div>
+                      <div className="my-2 flex items-center">
+                        <Label className="w-1/3">City: </Label>
+                        <Input
+                          type="text"
+                          name="address"
+                          className="my-2 w-3/4"
+                        />
+                      </div>
+                      <div className="my-2 flex items-center">
+                        <Label className="w-1/3">Province: (e.g. ON) </Label>
+                        <Input
+                          type="text"
+                          name="address"
+                          className="my-2  w-3/4"
+                        />
+                      </div>
+                      <div className="my-2 flex items-center">
+                        <Label className="w-1/3">Postal Code </Label>
+                        <Input
+                          type="text"
+                          name="address"
+                          className="my-2  w-3/4"
+                        />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex items-center justify-around">
-              <button className="h-1/4 text-primary" type="submit">
-                Add new address
-              </button>
-            </div>
-          </form>
-        )}
-        <span className="mt-5 flex justify-between">
-          <ReturnProcessBackButton onClick={() => returnProcess.back()} />
+                <div className="flex items-center justify-around">
+                  <button className="h-1/4 text-primary" type="submit">
+                    Add new address
+                  </button>
+                </div>
+              </form>
+            )}
+            <span className="mt-5 flex justify-between">
+              <ReturnProcessBackButton onClick={() => returnProcess.back()} />
 
-          <ReturnProcessNextButton onClick={() => returnProcess.forward()} />
-        </span>
-      </div>
-
-      {/* {step === 2 && (<div><div className="text-brand">
-        Which pickup method do you prefer?
-      </div>
-        <form id="selectPickupMethod" className="mt-5 flex justify-center">
-          <div className="flex flex-row justify-center mx-1/5 w-3/4">
-            <div className="justify-center border-4 border-brand rounded-[12px] mx-4  py-2 px-4 max-w-[40%]">
-              <div className="flex justify-center text-smallText font-bold ">
-                Direct Handoff </div>
-              <div className="flex justify-center "> Hand the package directly to our specialist at your door </div>
-              <div className="flex justify-center" >
-                <Input type="radio" key="handoff" value="handoff" name="pickupMethod" checked={selectedMethod === 'handoff'}
-                  onChange={handleRadioChange} />
-              </div>
-            </div>
-
-            <div className="justify-center border-4 border-brand rounded-[12px] mx-4 py-2 px-4 max-w-[40%]">
-              <div className="flex justify-center text-smallText font-bold ">
-                Leave on Doorstep </div>
-              <div className="flex justify-center"> Place items outside your door ahead of your pick up window </div>
-              <div className="flex justify-center">
-
-                <Input type="radio" key="doorstep" value="doorstep" name="pickupMethod" checked={selectedMethod === 'doorstep'}
-                  onChange={handleRadioChange} />
-              </div>
-            </div>
+              <ReturnProcessNextButton
+                onClick={() => returnProcess.forward()}
+              />
+            </span>
           </div>
-        </form>
-
-
-        <span className="flex justify-between mt-5" >
-          <Button className="bg-transparent hover:bg-transparent text-primary font-bold" onClick={() => setStep(1)}>← Back </Button>
-          <Button className="next text-white font-bold" onClick={() => setStep(3)} > Next → </Button>
-        </span></div>)} */}
-    </div>
+        </div>
+      </form>
+    </Form>
   )
 }
