02eabbe2bcfb343e4646c0b91a0ef1fb089f6a48
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-26 16:22:59 -0400

    Return date selection functionality to pick date page

--- src/test_pages/pick-date.tsx
+++ src/test_pages/pick-date.tsx
@@ -4,13 +4,7 @@ import {
   SectionHeader,
   SectionHeaderHighlight,
 } from '@/components/home/Home'
-import { Card, CardContent } from '@/components/ui/card'
-import { cn, getDayName } from '@/lib/utils'
-import {
-  faChevronLeft,
-  faChevronRight,
-  faClose,
-} from '@fortawesome/free-solid-svg-icons'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 import {
   Form,
   FormControl,
@@ -29,39 +23,11 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useDateSelection } from '@/hooks/useDateSelection'
 import { ReturnProcessNextButton } from '@/components/ui/common'
-
-type PickCardType = React.HTMLAttributes<HTMLDivElement> & {
-  date: Date
-}
-// TODO: Selecting a card moves all the other cards down, make sure only the selected card grows and the other ones don't move
-
-const PickDateCard = React.forwardRef<HTMLDivElement, PickCardType>(
-  // eslint-disable-next-line react/prop-types
-  ({ date, className, ...props }, ref) => {
-    return (
-      <Card
-        className={cn(
-          'w-[9.5rem] select-none border-brand bg-paleBlue text-brand hover:cursor-pointer data-[state=on]:scale-110 data-[state=on]:border-8 data-[state=on]:border-primary data-[state=on]:bg-white data-[state=on]:shadow-2xl',
-          className
-        )}
-        ref={ref}
-        {...props}
-      >
-        <CardContent className="flex flex-col items-center space-y-4 pt-6">
-          <p className="text-2xl font-semibold">
-            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */}
-            {getDayName(date.getDay())?.substring(0, 3)}
-          </p>
-          <p className="text-5xl font-bold">{date.getDate()}</p>
-          <p className="flex text-lg font-semibold">
-            {date.toLocaleString('en-us', { month: 'short', year: 'numeric' })}
-          </p>
-        </CardContent>
-      </Card>
-    )
-  }
-)
-PickDateCard.displayName = 'PickDateCard'
+import {
+  LastWeekButton,
+  NextWeekButton,
+  PickDateCard,
+} from '@/components/PickDate/PickDate'
 
 export default function PickDate() {
   const returnProcess = useReturnProcess()
@@ -84,6 +50,14 @@ export default function PickDate() {
     console.log('Submitted:', values)
     returnProcess.setCurrentData(values)
     returnProcess.forward()
+  }
+
+  function weekForwards() {
+    dateSelection.forward()
+  }
+
+  function weekBackwards() {
+    dateSelection.back()
   }
 
   return (
@@ -122,38 +96,16 @@ export default function PickDate() {
             </HomeSection>
 
             <div className="flex-row justify-center gap-x-4 space-y-11">
-              {/* <div className="flex  items-center justify-center space-y-4 text-center font-semibold text-brand"> */}
               <div className="flex justify-center gap-x-11 xl:hidden">
-                <div className="flex flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary">
-                  <FontAwesomeIcon
-                    size="2x"
-                    width={'50'}
-                    height={'60'}
-                    icon={faChevronLeft}
-                  />
-                  <p className="text-2xl">Last Week</p>
-                </div>
-                <div className="flex flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary">
-                  <FontAwesomeIcon
-                    size="2x"
-                    width={'50'}
-                    height={'60'}
-                    icon={faChevronRight}
-                  />
-                  <p className="text-2xl">Last Week</p>
-                </div>
+                <LastWeekButton
+                  size="default"
+                  onClick={() => weekBackwards()}
+                />
+                <NextWeekButton size="default" onClick={() => weekForwards()} />
               </div>
 
               <div className="flex">
-                {/* <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
-              <FontAwesomeIcon
-                size="2x"
-                width={'30'}
-                height={'45'}
-                icon={faChevronLeft}
-              />
-              <p className="text-xl">Last Week</p>
-            </div> */}
+                <LastWeekButton size="small" onClick={() => weekBackwards()} />
 
                 <FormField
                   control={form.control}
@@ -187,16 +139,7 @@ export default function PickDate() {
                     </FormItem>
                   )}
                 />
-                {/* 
-                <div className="hidden w-fit flex-col items-center justify-center space-y-4 text-center font-semibold text-brand hover:cursor-pointer hover:text-primary xl:flex">
-                  <FontAwesomeIcon
-                    size="2x"
-                    width={'30'}
-                    height={'45'}
-                    icon={faChevronRight}
-                  />
-                  <p className="text-xl">Last Week</p>
-                </div> */}
+                <NextWeekButton size="small" onClick={() => weekForwards()} />
               </div>
             </div>
             <span className="mt-5 flex justify-end">
