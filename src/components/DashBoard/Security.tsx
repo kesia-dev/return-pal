093f66a001d05b268ad2097d0aba8d8a238d054a
javascriptsucks <evix1988@live.cn>
2023-10-27 18:06:47 -0600

    created form for password reset in security component in Dashboard page

--- src/components/DashBoard/Security.tsx
+++ src/components/DashBoard/Security.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import DashBoardHeader from '@/components/DashBoard/DashBoardHeader'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+
+const securityFormSchema = z.object({
+  email: z.string().email({ message: 'Please enter a valid email' }),
+})
 
 function Security() {
+  const form = useForm<z.infer<typeof securityFormSchema>>({
+    resolver: zodResolver(securityFormSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof securityFormSchema>) {
+    console.log(values)
+  }
+
   return (
     <>
       <DashBoardHeader
@@ -9,9 +38,35 @@ function Security() {
         lastName="Doe"
         email="john@example.com"
       />
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Security</h1>
-        <p className="text-xl">Coming soon...</p>
+      <div className="flex flex-col items-center justify-center text-brand">
+        <h1 className="text-title font-bold">Security</h1>
+        <div className="flex flex-col items-center justify-center">
+          <label className="block">Reset Password:</label>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Please Enter Email to Reset Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </>
   )
