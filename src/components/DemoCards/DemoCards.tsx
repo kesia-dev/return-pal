92701a0024ca7b3df35f5434dbb9465dd785e155
javascriptsucks <evix1988@live.cn>
2023-10-31 10:51:22 -0600

    change layout of svg background responsed to the Card size changes

--- src/components/DemoCards/DemoCards.tsx
+++ src/components/DemoCards/DemoCards.tsx
@@ -59,7 +59,7 @@ const cardData2 = [
 
 function DemoCards() {
   return (
-    <div className="absolute top-64 w-full space-y-12 py-2.5 text-center leading-4 text-white md:top-56">
+    <div className="absolute top-56 w-full space-y-12 py-2.5 text-center leading-4 text-white md:top-28 lg:top-32 xl:top-24">
       <section className="xl:px-18 px-10 md:px-7 lg:px-16">
         <div className="mx-auto mt-20 flex max-w-7xl justify-between gap-6 md:mt-10 lg:mt-8">
           <title className="mb-4 block max-w-5xl text-left text-mediumText lg:text-3xl xl:text-subtitle">
