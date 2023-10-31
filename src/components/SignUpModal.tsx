d11c81100459bd6b152e5b3bdc842d871b727b21
mike-dalton <michael.j.dalton@gmail.com>
2023-10-31 11:09:52 -0400

    start signup page

---
+++ src/components/SignUpModal.tsx
@@ -0,0 +1,97 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@components/ui/form'
+import { Input } from '@components/ui/input'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { Button } from '@components/ui/button'
+import Link from 'next/link'
+import NextArrow from '@components/SvgComponents/NextArrow'
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Please enter a valid email' }),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+})
+
+function SignInForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-2 flex flex-col items-center justify-start"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="my-4 h-10 w-[200px] rounded-xl border-4 border-primary text-lg placeholder:text-grey sm:h-14 sm:w-[275px]"
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="my-4 h-10 w-[200px] rounded-xl border-4 border-primary text-lg placeholder:text-grey sm:h-14 sm:w-[275px]"
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Link
+          href="/sign-in"
+          className="mb-5 mt-1 font-semibold text-grey underline"
+        >
+          Forgot your password?
+        </Link>
+        <Button
+          type="submit"
+          className="h-10 w-[150px] rounded-3xl text-lg sm:h-12 sm:w-[150px]"
+        >
+          Sign In&nbsp;&nbsp;
+          <NextArrow />
+        </Button>
+        <p className="my-8 font-semibold text-grey">
+          Don&apos;t have an account yet?
+          <Link href="/sign-in" className="font-semibold text-primary">
+            <span>&nbsp;Sign Up</span>
+          </Link>
+        </p>
+      </form>
+    </Form>
+  )
+}
