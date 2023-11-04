37f3d400b49a9757a9d0da604fa5234e6275a2e9
mike-dalton <michael.j.dalton@gmail.com>
2023-11-04 08:15:03 -0400

    reformat and fade-in contact us page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,42 +2,48 @@ import React from 'react'
 import ContactForm from '@/components/ContactForm/ContactForm'
 import Image from 'next/image'
 import DefaultLayout from '@/layouts/DefaultLayout'
-import Link from 'next/link'
+import Reveal from '@components/common/reveal'
 
 function Contact() {
   return (
-    <div className="mt-8 flex">
-      <section className="mx-auto flex w-3/4 flex-col space-y-8 lg:mx-0 lg:ml-24 lg:max-w-3xl xl:max-w-5xl">
-        <h2 className="text-subtitle">
-          <span className="mr-2 font-thin text-brand">CONTACT</span>
-          <span className="bg-gradient-to-r from-gradientL to-primary bg-clip-text font-bold text-transparent">
-            US
-          </span>
-        </h2>
+    <div className="mt-8 flex justify-end lg:w-1/2">
+      <section className="flex w-5/6 flex-col space-y-8">
+        <Reveal>
+          <h2 className="text-subtitle">
+            <span className="mr-2 font-thin text-brand">CONTACT</span>
+            <span className="bg-gradient-to-r from-gradientL to-primary bg-clip-text font-bold text-transparent">
+              US
+            </span>
+          </h2>
+        </Reveal>
         <ContactForm />
-        <Link href="/">
+        <Reveal width="100%">
           <Image
-            className="my-4 pl-10"
-            src={'/images/returnpal-short-logo.png'}
+            className="mx-16 mb-6 hidden self-center md:mx-auto md:block"
+            src="/images/robot.png"
             alt="Contact Us"
-            width={160}
-            height={0}
+            width={240}
+            height={240}
           />
-        </Link>
+        </Reveal>
+        {/* <Link href="/">
+          <Reveal>
+            <Image
+              className="my-4 pl-10"
+              src={'/images/returnpal-short-logo.png'}
+              alt="Contact Us"
+              width={160}
+              height={0}
+            />
+          </Reveal>
+        </Link> */}
       </section>
-      <Image
-        className="mx-16 hidden self-center md:mx-auto md:block"
-        src="/images/robot.png"
-        alt="Contact Us"
-        width={160}
-        height={160}
-      />
     </div>
   )
 }
 Contact.getLayout = (page: React.ReactElement) => {
   return (
-    <DefaultLayout isHeaderShow={true} isFooterShow={false}>
+    <DefaultLayout isHeaderShow={true} isFooterShow={true}>
       {page}
     </DefaultLayout>
   )
