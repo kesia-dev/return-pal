d0d2a6c12bf62e8b3a27af0dbd12b2a15e0a6446
mike-dalton <michael.j.dalton@gmail.com>
2023-11-04 14:28:55 -0400

    fade-in dashboard/main

--- src/components/DashBoard/DashBoardMain.tsx
+++ src/components/DashBoard/DashBoardMain.tsx
@@ -4,32 +4,45 @@ import NounDelivery from '@/components/SvgComponents/NounDelivery'
 import Link from 'next/link'
 import { type UserInfo } from '@/components/DashBoard/types'
 import { planTextClassName } from '@/components/Plan'
+import Reveal from '@components/common/reveal'
 
 function DashBoardMain({ userInfo }: { userInfo: UserInfo }) {
   return (
     <section className="flex flex-col justify-center space-y-16 p-36">
-      <h3 className="text-title">
-        Welcome back,{' '}
-        <span
-          className={`${planTextClassName(
-            userInfo.role
-          )} text-title sm:text-title`}
-        >
-          {userInfo.role}
-        </span>{' '}
-        user <span className="font-bold text-primary">John</span>
-      </h3>
-      <div className="flex rounded-3xl bg-brand p-8 text-subtitle">
-        <div className="flex w-4/5 flex-col space-y-5">
-          <h3 className="text-start text-white">
-            Ready to schedule your pickup?
-          </h3>
-          <Link href={'/return'}>
-            <Button className="w-fit px-8">Schedule Pickup</Button>
-          </Link>
+      <Reveal>
+        <h3 className="text-title">
+          Welcome back,{' '}
+          <span
+            className={`${planTextClassName(
+              userInfo.role
+            )} text-title sm:text-title`}
+          >
+            {userInfo.role}
+          </span>{' '}
+          user <span className="font-bold text-primary">John</span>
+        </h3>
+      </Reveal>
+      <Reveal>
+        <div className="flex rounded-3xl bg-brand p-8 text-subtitle">
+          <div className="flex w-4/5 flex-col space-y-5">
+            <Reveal>
+              <h3 className="text-start text-white">
+                Ready to schedule your pickup?
+              </h3>
+            </Reveal>
+            <Link href={'/return'}>
+              <Reveal>
+                <Button className="w-fit px-8">Schedule Pickup</Button>
+              </Reveal>
+            </Link>
+          </div>
+          <div className="h-auto w-auto">
+            <Reveal width="100%">
+              <NounDelivery />
+            </Reveal>
+          </div>
         </div>
-        <NounDelivery />
-      </div>
+      </Reveal>
     </section>
   )
 }
