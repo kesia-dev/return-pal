f4fe600953952919b78059ef82c00107e629e738
Michael Le <72768694+mileuc@users.noreply.github.com>
2024-01-05 15:38:47 -0700

    Update Dashboard Design

--- src/components/DashBoard/DashBoardMain.tsx
+++ src/components/DashBoard/DashBoardMain.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import NounDelivery from '@/components/SvgComponents/NounDelivery'
+import Link from 'next/link'
 import { type UserInfo } from '@/components/DashBoard/types'
 import Reveal from '@components/common/reveal'
 import {
@@ -7,11 +10,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import HandingPackage from '@/components/SvgComponents/HandingPackage'
 import { useRouter } from 'next/navigation'
-// import Profile from '@/components/DashBoard/Profile'
+import Profile from './Profile'
 import { CiCalendar } from 'react-icons/ci'
 import { FaRegCircleUser, FaRegClock } from 'react-icons/fa6'
-import RecentOrders from '@components/Orders/RecentOrders'
+
 function DashBoardMain({
   userInfo,
   setUserInfo,
@@ -19,124 +23,134 @@ function DashBoardMain({
   userInfo: UserInfo
   setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>
 }) {
+  const [showProfile, setShowProfile] = useState(false)
+
   const cardClassnames =
-    'border-l-0 border-r-0 border-t-0 border-b-0 border-black flex h-23 h-23 sm:w-1/3 md:w-1/3 lg:w-1/4  select-none flex-row items-center p-1 bg-white mr-8 text-brand '
-  const cardTitleClassnames =
-    'text-xl sm:text-base md:text-l lg:text-xl font-semibold '
-  const cardDescriptionClassNames =
-    'text-sm text-brand sm:text-base md:text-l lg:text-xl'
+    'border-l-0 border-r-0 border-t-0 border-b-0  border-black flex h-45 md:w-1/3 select-none flex-row items-center p-4 bg-white text-brand'
+  const cardTitleClassnames = 'text-xl md:text-2xl font-semibold'
+
+  const cardDescriptionClassNames = 'text-sm text-brand'
+
   const router = useRouter()
+
   const handleRedirect = (path: string) => {
     router.push(path)
   }
 
+  const handleProfileClick = () => {
+    setShowProfile(true)
+  }
+
   return (
-    <section className="lg:p-30 flex flex-col justify-center space-y-8 pb-10 pl-20 pr-20 pt-10 lg:space-y-16">
-      <div>
-        <Reveal>
-          <h3 className="mb-6 text-subtitle font-bold lg:text-4xl">
-            Your Dashboard
-          </h3>
-        </Reveal>
-        <div className="justify-left mb-12 flex">
-          <Card
-            onClick={() => handleRedirect('/return')}
-            className={`${cardClassnames}`}
-            style={{
-              borderLeft: '16px solid black',
-              minWidth: '335px',
-              minHeight: '150px',
-              maxHeight: '150px',
-            }}
-          >
-            <CardHeader className="flex items-center pl-5">
-              <Reveal>
-                <div className="flex items-center">
-                  <div className="mr-4">
-                    <Reveal>
-                      <CiCalendar className="fill-secondary xs:h-19 xs:w-19 h-12 w-12 xxs:h-16 xxs:w-16" />
-                    </Reveal>
+    <section className="lg:p-30 flex flex-col justify-center space-y-8  p-20 lg:space-y-16">
+      {!showProfile && (
+        <div>
+          <Reveal>
+            <h3 className="mb-6 text-subtitle font-bold lg:text-title">
+              Your Dashboard
+            </h3>
+          </Reveal>
+
+          <div className="justify-left mb-12 flex">
+            <Card
+              onClick={() => handleRedirect('/return')}
+              className={`${cardClassnames} mr-4`}
+              style={{ borderLeft: '16px solid black' }}
+            >
+              <CardHeader className="flex items-center pl-5">
+                <Reveal>
+                  <div className="flex items-center">
+                    {' '}
+                    {/* Added a container */}
+                    <div className="mr-4">
+                      {' '}
+                      {/* Container for the icon */}
+                      <Reveal>
+                        <CiCalendar className="fill-secondary h-12 w-12 xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" />
+                        {/* <HandingPackage className="h-12 w-12 fill-primary xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" /> */}
+                      </Reveal>
+                    </div>
+                    <div>
+                      <CardTitle className={cardTitleClassnames}>
+                        Schedule Pickup
+                      </CardTitle>
+                      <Reveal>
+                        <CardDescription className={cardDescriptionClassNames}>
+                          Set up a new pickup for your returns.
+                        </CardDescription>
+                      </Reveal>
+                    </div>
                   </div>
-                  <div>
-                    <CardTitle className={cardTitleClassnames}>
-                      Schedule Pickup
-                    </CardTitle>
-                    <Reveal>
-                      <CardDescription className={cardDescriptionClassNames}>
-                        Set up a new pickup for your returns.
-                      </CardDescription>
-                    </Reveal>
+                </Reveal>
+              </CardHeader>
+            </Card>
+
+            <Card
+              onClick={() => handleRedirect('/return')}
+              className={`${cardClassnames} mr-4`}
+              style={{ borderLeft: '16px solid black' }}
+            >
+              <CardHeader className="flex items-center pl-5">
+                <Reveal>
+                  <div className="flex items-center">
+                    {' '}
+                    {/* Added a container */}
+                    <div className="mr-4">
+                      {' '}
+                      {/* Container for the icon */}
+                      <Reveal>
+                        {/* <HandingPackage className="h-12 w-12 fill-primary xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" /> */}
+                        <FaRegClock className="fill-secondary h-12 w-12 xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" />
+                      </Reveal>
+                    </div>
+                    <div>
+                      <CardTitle className={cardTitleClassnames}>
+                        View Recent Orders
+                      </CardTitle>
+                      <Reveal>
+                        <CardDescription className={cardDescriptionClassNames}>
+                          View all your recently scheduled orders here.
+                        </CardDescription>
+                      </Reveal>
+                    </div>
                   </div>
-                </div>
-              </Reveal>
-            </CardHeader>
-          </Card>
-          <Card
-            onClick={() => handleRedirect('/return')}
-            className={`${cardClassnames}`}
-            style={{
-              borderLeft: '16px solid black',
-              minWidth: '335px',
-              minHeight: '150px',
-              maxHeight: '150px',
-            }}
-          >
-            <CardHeader className="flex items-center pl-5">
-              <Reveal>
-                <div className="flex items-center">
-                  <div className="mr-4">
-                    <Reveal>
-                      <FaRegClock className="fill-secondary xs:h-19 xs:w-19 h-12 w-12 xxs:h-16 xxs:w-16" />
-                    </Reveal>
+                </Reveal>
+              </CardHeader>
+            </Card>
+
+            <Card
+              onClick={handleProfileClick}
+              className={`${cardClassnames}`}
+              style={{ borderLeft: '16px solid black' }}
+            >
+              <CardHeader className="flex items-center pl-5">
+                <Reveal>
+                  <div className="flex items-center">
+                    <div className="mr-4">
+                      <Reveal>
+                        <FaRegCircleUser className="h-12 w-12 fill-secondary xxs:h-16 xxs:w-16 xs:h-24 xs:w-24"/>
+                        {/* <HandingPackage className="h-12 w-12 fill-primary xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" /> */}
+                      </Reveal>
+                    </div>
+                    <div>
+                      <CardTitle className={cardTitleClassnames}>
+                        Manage Account
+                      </CardTitle>
+                      <Reveal>
+                        <CardDescription className={cardDescriptionClassNames}>
+                          Edit your profile setting and subscriptions here.
+                        </CardDescription>
+                      </Reveal>
+                    </div>
                   </div>
-                  <div>
-                    <CardTitle className={cardTitleClassnames}>
-                      View Recent Orders
-                    </CardTitle>
-                    <Reveal>
-                      <CardDescription className={cardDescriptionClassNames}>
-                        View all your recently scheduled orders here.
-                      </CardDescription>
-                    </Reveal>
-                  </div>
-                </div>
-              </Reveal>
-            </CardHeader>
-          </Card>
-          <Card
-            className={`${cardClassnames}`}
-            style={{
-              borderLeft: '16px solid black',
-              minWidth: '335px',
-              minHeight: '150px',
-              maxHeight: '150px',
-            }}
-          >
-            <CardHeader className="flex items-center pl-5">
-              <Reveal>
-                <div className="flex items-center">
-                  <div className="mr-4">
-                    <Reveal>
-                      <FaRegCircleUser className="fill-secondary xs:h-19 xs:w-19 h-12 w-12 xxs:h-16 xxs:w-16" />
-                    </Reveal>
-                  </div>
-                  <div>
-                    <CardTitle className={cardTitleClassnames}>
-                      Manage Account
-                    </CardTitle>
-                    <Reveal>
-                      <CardDescription className={cardDescriptionClassNames}>
-                        Edit your profile setting and subscriptions here.
-                      </CardDescription>
-                    </Reveal>
-                  </div>
-                </div>
-              </Reveal>
-            </CardHeader>
-          </Card>
+                </Reveal>
+              </CardHeader>
+            </Card>
+          </div>
+          <h4 className="text-subtitle font-bold">Recent Orders</h4>
         </div>
-        <RecentOrders />
-      </div>
+      )}
+      {showProfile && <Profile userInfo={userInfo} setUserInfo={setUserInfo} />}
     </section>
   )
 }
