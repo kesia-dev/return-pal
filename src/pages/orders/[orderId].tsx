a087c93740a95f9bfa1c325452f3d666b87de4b4
Bruce Lee <brucehlee@yahoo.ca>
2024-01-15 14:01:14 -0500

    Updated manage order, order status pages

--- src/pages/orders/[orderId].tsx
+++ src/pages/orders/[orderId].tsx
@@ -1,80 +1,23 @@
-// import { useRouter } from 'next/router'
-// import { useEffect, useState } from 'react'
-// import { type Order } from '@components/DashBoard/types'
-
-// const OrderId: React.FC = () => {
-//   const router = useRouter()
-//   const { orderId } = router.query
-//   const [order, setOrder] = useState<Order | null>(null)
-
-//   useEffect(() => {
-//     const fetchOrderDetails = async () => {
-//       try {
-//         if (orderId) {
-//           console.log('orderId : ', orderId)
-//           const orderIdString = Array.isArray(orderId) ? orderId[0] : orderId
-//           const response = await fetch(`/api/orders/${orderIdString}`)
-//           console.log('response is :: ', response)
-//           if (response.ok) {
-//             console.log('response is ok')
-//             const data = (await response.json()) as Order
-//             setOrder(data)
-//           } else {
-//             console.error('Error fetching order details', response.statusText)
-//           }
-//         }
-//       } catch (error) {
-//         console.error('Error fetching order details', error)
-//       }
-//     }
-
-//     ;(async () => {
-//       await fetchOrderDetails()
-//     })().catch((error) => {
-//       console.error('Error in fetchOrderDetails:', error)
-//     })
-//   }, [orderId])
-
-//   return (
-//     <div style={{ padding: '20px' }}>
-//       <h1>Manage Order Page</h1>
-//       {order ? (
-//         <>
-//           <h2>Order Details</h2>
-//           <p>Order Number: {order.order_number}</p>
-//           <p>Item Name: {order.itemName}</p>
-//           <p>Quantity: {order.quantity}</p>
-//           <p>Orders Status: {order.status}</p>
-//           <p>
-//             Orders Placed on: {order.order_date?.$dateFromString?.dateString}
-//           </p>
-//         </>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default OrderId
 // pages/orders/[orderId].tsx
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import Link from 'next/link'
 import { VscArchive, VscCreditCard } from 'react-icons/vsc'
 import { IconContext } from 'react-icons'
 import DefaultLayout from '@/layouts/DefaultLayout'
 import OrderStatusNodes from '@/components/Orders/OrderStatusNodes'
-
 import { type Order } from '@/components/DashBoard/types'
 import { Button } from '@/components/ui/button'
 import ConfirmationDialog from '@components/Orders/ConfirmationDialog'
+
 const OrderId = () => {
   const [selectedOrder, setSelectedOrder] = useState<Order | null>(null)
   const router = useRouter()
   const { orderId } = router.query
   const [order, setOrder] = useState<Order | null>(null)
-
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
   useEffect(() => {
     const fetchOrderDetails = async () => {
       try {
@@ -171,6 +114,7 @@ const OrderId = () => {
                 <div className="text-black-900 font-avenir-next with flex items-center space-x-10 text-smallText font-bold">
                   6500 Boulevard de Rome, Brossard, QC, J4Y 0B6
                 </div>
+
                 <OrderStatusNodes status={order.status} />
               </div>
               {/* Right side */}
@@ -185,7 +129,7 @@ const OrderId = () => {
                 {/* Total packages */}
                 <div className=" flex items-center space-x-4">
                   <IconContext.Provider value={{ size: '1.5em' }}>
-                    <VscArchive />
+                    <VscArchive style={{ color: '008BE6' }} />
                   </IconContext.Provider>
                   <span className="w-{80} text-black-900 font-avenir-next with p-sm flex items-center text-smallText font-bold">
                     Total Packages:
@@ -197,7 +141,7 @@ const OrderId = () => {
                 {/* Visa information */}
                 <div className="flex items-center space-x-4">
                   <IconContext.Provider value={{ size: '1.5em' }}>
-                    <VscCreditCard />
+                    <VscCreditCard style={{ color: '008BE6' }} />
                   </IconContext.Provider>
                   <span className="text-black-900 font-avenir-next with flex items-center text-smallText font-bold">
                     Visa ending in:
@@ -210,28 +154,46 @@ const OrderId = () => {
             </div>
           </div>
           <div
-            className="mt-4 grid grid-cols-2 gap-4"
+            className=" mt-4 grid grid-cols-2 gap-4"
             style={{ gridTemplateColumns: '1fr auto', marginLeft: '800px' }}
           >
             <div>
               <Link href="/orders">
-                <Button>Back</Button>
+                <Button className="  ">Back</Button>
               </Link>
+
               <Button
-                className="ring-offset-background focus-visible:ring-ring inline-flex h-10 items-center justify-center whitespace-nowrap rounded-full border-2 border-primary bg-white px-4 py-2 text-sm font-bold text-primary transition-colors hover:bg-slate-100/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 active:scale-95 active:shadow-none active:ring-0 active:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80"
+                className=" ring-offset-background focus-visible:ring-ring inline-flex h-10 items-center justify-center whitespace-nowrap rounded-full border-2 border-primary bg-white px-4 py-2 text-sm font-bold text-primary transition-colors hover:bg-slate-100/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 active:scale-95 active:shadow-none active:ring-0 active:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80"
                 onClick={() => handleCancelOrder(order._id, order.order_number)}
                 style={{
                   opacity:
-                    order.status === 'Cancelled' || order.status === 'Delivered'
+                    order.status === 'Cancelled' ||
+                    order.status === 'Delivered' ||
+                    order.status === 'OrderPlaced' ||
+                    order.status === 'PickedUp' ||
+                    order.status === 'InTransit' ||
+                    order.status === 'Returned' ||
+                    order.status === 'OrderCancelled'
                       ? '2.0'
                       : '1',
                   cursor:
-                    order.status === 'Cancelled' || order.status === 'Delivered'
+                    order.status === 'Cancelled' ||
+                    order.status === 'Delivered' ||
+                    order.status === 'OrderPlaced' ||
+                    order.status === 'PickedUp' ||
+                    order.status === 'InTransit' ||
+                    order.status === 'Returned' ||
+                    order.status === 'OrderCancelled'
                       ? 'not-allowed'
                       : 'pointer',
                 }}
                 disabled={
-                  order.status === 'Cancelled' || order.status === 'Delivered'
+                  order.status === 'Cancelled' ||
+                  order.status === 'Delivered' ||
+                  order.status === 'OrderPlaced' ||
+                  order.status === 'PickedUp' ||
+                  order.status === 'InTransit' ||
+                  order.status === 'OrderCancelled'
                 }
               >
                 Cancel Order
