c2a2a2f43de6c5ff36a75b4826525f6674c0859b
Bruce Lee <brucehlee@yahoo.ca>
2024-01-22 14:53:14 -0500

    Committed [orderId].tsx OrderStatusNodes.tsx

--- src/components/Orders/OrderStatusNodes.tsx
+++ src/components/Orders/OrderStatusNodes.tsx
@@ -2,18 +2,15 @@
 import { IoCheckmarkCircle } from 'react-icons/io5'
 import { cn } from '@lib/utils'
 import React from 'react'
-
 interface OrderStatusNodeProps {
   label: string
   isActive: boolean
 }
-
 const OrderStatusNode: React.FC<OrderStatusNodeProps> = ({
   label,
   isActive,
 }) => {
   console.log('isActive:', true) // Add this line
-
   return (
     <div className={'w-1/5 pl-0 pr-0 pt-5'}>
       <div className="relative mb-4">
@@ -23,7 +20,6 @@ const OrderStatusNode: React.FC<OrderStatusNodeProps> = ({
         >
           <div className="align-center z-10 h-1 w-full flex-1 items-center rounded bg-primary align-middle "></div>
         </div>
-
         <div
           className={cn(
             ' z-30 mx-auto flex h-10 w-10 items-center rounded-full border-2 border-primary text-lg text-white'
@@ -47,11 +43,9 @@ const OrderStatusNode: React.FC<OrderStatusNodeProps> = ({
     </div>
   )
 }
-
 interface OrderStatusNodesProps {
   status: string | undefined
 }
-
 const OrderStatusNodes: React.FC<OrderStatusNodesProps> = ({ status }) => (
   <div className="mt-16 flex items-center justify-center">
     <div className="order-status-nodes-container flex ">
@@ -59,22 +53,18 @@ const OrderStatusNodes: React.FC<OrderStatusNodesProps> = ({ status }) => (
         label="Driver received"
         isActive={status === `Driver received` || false}
       />
-
       <OrderStatusNode
         label="Driver on the way"
         isActive={status === 'Driver on the way' || true}
       />
-
       <OrderStatusNode
         label="Driver delivered to post office"
         isActive={status === 'Driver delivered to post office' || true}
       />
-
       <OrderStatusNode
         label="Delivered"
         isActive={status === 'Delivered' || false}
       />
-
       <OrderStatusNode
         label="Cancelled"
         isActive={status === 'Cancelled' || false}
