5f0fdcbdba034fc65b6480f51a0b2817eea8d7db
luwam <lwamyohannes2018@gmail.com>
2024-02-08 09:29:25 -0700

    fixed bug in manage-account.tsx

---
+++ src/pages/api/manage-account/dummy-data.tsx
@@ -0,0 +1,10 @@
+import { ObjectId } from 'mongodb'
+
+export const dummyData = {
+  first_name: "John",
+  last_name: "Doe",
+  defaultAddress: "123 Main St 12A, Toronto, ON, M1M1M1",
+  phoneNumber: "123-456-7890",
+  emailAddress: "john.doe@example.com",
+  password: "password123"
+}
