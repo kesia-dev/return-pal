65d6657eeaaab60109cd8556c08bf15bcfdd94bf
luwam <lwamyohannes2018@gmail.com>
2024-02-14 23:19:55 -0700

    fix navigation button to Manage Account page on dashboard

--- src/pages/api/manage-account/editProfile.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import client, { connectDB, disconnectDB } from '@/lib/db'
-import type { UserInfo, Address } from '@/components/DashBoard/types'
-import { ObjectId } from 'mongodb'
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {}
