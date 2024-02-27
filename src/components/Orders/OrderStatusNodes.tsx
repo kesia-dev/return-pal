992aaf1acd4e4e38c1a5fecebd6141b966984cb8
Bruce Lee <brucehlee@yahoo.ca>
2024-02-26 22:04:30 -0500

    Updated files

--- src/components/Orders/OrderStatusNodes.tsx
+++ src/components/Orders/OrderStatusNodes.tsx
@@ -18,7 +18,7 @@ const OrderStatusNode: React.FC<OrderStatusNodeProps> = ({
           >
             <div className="align-center z-10 h-1 w-full flex-1 items-center rounded bg-primary align-middle "></div>
           </div>
-          <div className="z-30 mx-auto flex h-10 w-10 items-center rounded-full border-2 border-primary text-lg text-white">
+          <div className="z-30 mx-auto flex h-8 w-8 items-center rounded-full border-2 border-primary text-lg text-white">
             <IoCheckmarkCircle size={40} className="rounded-full bg-primary" />
           </div>
           <p className="mb-2 w-full select-none space-x-10 pt-3 text-center text-xl text-black md:text-base">
@@ -32,9 +32,9 @@ const OrderStatusNode: React.FC<OrderStatusNodeProps> = ({
             className="align-center absolute top-5 flex translate-x-[-50%] translate-y-[-50%] transform content-center items-center align-middle "
             style={{ width: '70%' }}
           >
-            <div className="align-center z-10 h-1 w-full flex-1 items-center rounded bg-gray-300 align-middle "></div>
+            <div className="align-center z-10 mb-2 h-1 w-full flex-1 items-center rounded bg-gray-300 align-middle "></div>
           </div>
-          <div className="z-30 mx-auto flex h-10 w-10 items-center rounded-full border-2 border-gray-300 text-lg text-white">
+          <div className="z-30 mx-auto flex h-8 w-8 items-center rounded-full border-2 border-gray-300 text-lg text-white">
             {/* Placeholder for hidden checkmark */}
           </div>
           <p className="mb-2 w-full select-none space-x-10 pt-3 text-center text-xl text-gray-500 md:text-base">
